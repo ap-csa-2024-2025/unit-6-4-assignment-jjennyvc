37827e7a5f153e679550d183a6ae39ffb05e7a18
jjennyvc <153659964+jjennyvc@users.noreply.github.com>
2025-02-04 19:14:23 +0000

    jjjj

--- Main.java
+++ Main.java
@@ -38,14 +38,41 @@ public class Main
   {
     for(int i = 0; i < arr.lenth; i++)
     {
-      if(arr[i].equals())
+      int target = arr[i];
+      for(int j = i+1; j < arr.length; j++)
+      {
+        if(arr[j] == (target))
+        {
+          return true;
+        }
+      }
     }
     return false;
   }
 
-  public static String findMode(String[] arr)
+  public static String findMode(int[] arr)
   {
-    // replace with your code
-    return null;
+    int count = 0;
+    int max_count = 0;
+    int mode = arr[0];
+    for (int i = 0; i < arr.length; i++)
+    {
+      int potential_mode = arr[i];
+      count = 0; 
+      for(int j = i+1; j < arr.length; j++)
+      {
+        if(arr[j] == arr[i])
+        {
+          count++;
+        }
+      }
+    }
+    if(count > max_count)
+    {
+      count = max_count;
+      mode = potential_mode
+    }
+  }
+    return mode;
   }
 }
